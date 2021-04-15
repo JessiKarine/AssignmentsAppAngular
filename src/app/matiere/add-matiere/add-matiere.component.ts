9cfd75b6649f7e2d5cf776f2338c6b9dcaaf9a5e
Rajaonarivelo TInasoa <rajaotix@gmail.com>
2021-04-15 22:01:28 +0300

    Merge branch 'develop' of https://gitlab.com/mevaElodie/angular-projet into develop

--- src/app/matiere/add-matiere/add-matiere.component.ts
+++ src/app/matiere/add-matiere/add-matiere.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatiereServiceService } from 'src/app/matiere-service.service';
+import { ProfService } from 'src/app/prof-service.service';
+import { Prof } from 'src/app/Prof/Prof.model';
+import { Matiere } from '../matiere.model';
 
 @Component({
   selector: 'app-add-matiere',
@@ -6,10 +11,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-matiere.component.css']
 })
 export class AddMatiereComponent implements OnInit {
-
-  constructor() { }
+  matiere:Matiere;
+  nom;
+  prof:Prof[] = [];
+  image;
+  idProf ;
+  constructor( private matiereService: MatiereServiceService,
+    private profService: ProfService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.getProfs();
+  }
+
+
+  onSubmit(event) {
+    if((!this.nom) || (!this.idProf)) return;
+
+    let newMatiere = new Matiere();
+    newMatiere.nom = this.nom;
+    newMatiere.prof = new Prof();
+    newMatiere.prof._id=this.idProf;
+    newMatiere.image = this.image;
+
+    this.matiereService.addMatiere(newMatiere)
+      .subscribe(reponse => {
+        console.log(reponse.message);
+
+         // et on navigue vers la page d'accueil qui affiche la liste
+         this.router.navigate(["/list-matiere"]);
+      });
+  }
+
+  getProfs(){
+    this.profService.getProfs()
+    .subscribe(data => {
+      console.log(data);
+      this.prof = data.docs;
+     
+    });
   }
 
 }
