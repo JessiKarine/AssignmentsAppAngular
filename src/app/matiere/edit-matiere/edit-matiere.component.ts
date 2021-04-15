3cbe0cac17b579c8a55a1146b6f25043f076f17b
jessi karine <razakasonjessi@gmail.com>
2021-04-15 19:28:48 +0300

    edit +liste deroulante

--- src/app/matiere/edit-matiere/edit-matiere.component.ts
+++ src/app/matiere/edit-matiere/edit-matiere.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatiereServiceService } from 'src/app/matiere-service.service';
+import { ProfService } from 'src/app/prof-service.service';
+import { Prof } from 'src/app/Prof/Prof.model';
+import { Matiere } from '../matiere.model';
 
 @Component({
   selector: 'app-edit-matiere',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditMatiereComponent implements OnInit {
 
-  constructor() { }
+  matiere:Matiere;
+  nom;
+  prof:Prof[] = [];
+  image;
+  idProf ;
+  constructor( private matiereService: MatiereServiceService,
+    private profService: ProfService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.getMatiereById();
+  }
+  onSubmit(event) {
+    // on va modifier l'assignment
+   // if((!this.nom) || (!this.idprof)) return;
+
+    this.matiere.nom = this.nom;
+   // this.matiere.prof = this.profService.getProf(this.idprof);
+    this.matiere.image = this.image;
+
+    this.matiereService.updateMatiere(this.matiere)
+      .subscribe(message => {
+        console.log("message "+message);
+
+        // et on navigue vers la page d'accueil
+        this.router.navigate(["/list-matiere"]);
+      })
+
   }
 
+  getMatiereById() {
+    // les params sont des string, on va forcer la conversion
+    // en number en mettant un "+" devant
+    const id  = this.route.snapshot.params.id;
+
+    console.log('Dans ngOnInit de details matiere, id = ' + id);
+    this.matiereService.getMatiere(id).subscribe((matiere) => {
+      this.nom = matiere.nom;
+      this.getProfs();
+      this.image = matiere.image;
+      this.matiere = matiere;
+      console.log("matiere : "+this.matiere.nom)
+    });
+  }
+
+  getProfs(){
+    this.profService.getProfs()
+    .subscribe(data => {
+      console.log(data);
+      this.prof = data.docs;
+     
+    });
+  }
 }
